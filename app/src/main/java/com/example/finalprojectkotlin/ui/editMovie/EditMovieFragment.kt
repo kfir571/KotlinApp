e6cdc6f59ada9af75cc12bad4d8a682c4126891e
Menashh <96311912+Menashh@users.noreply.github.com>
2023-04-24 15:19:03 +0300

    edit movie fix

--- app/src/main/java/com/example/finalprojectkotlin/ui/editMovie/EditMovieFragment.kt
+++ app/src/main/java/com/example/finalprojectkotlin/ui/editMovie/EditMovieFragment.kt
@@ -7,6 +7,7 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
@@ -18,6 +19,7 @@ import com.example.finalprojectkotlin.R
 import com.example.finalprojectkotlin.data.model.Movie
 import com.example.finalprojectkotlin.databinding.FragmentEditMovieBinding
 import com.example.finalprojectkotlin.ui.moviesViewModel
+import com.google.android.material.snackbar.Snackbar
 
 
 class EditMovieFragment : Fragment() { // binding is the connector between our UI and our data
@@ -26,6 +28,8 @@ class EditMovieFragment : Fragment() { // binding is the connector between our U
     private val binding get() = _binding!!
 
     private var imageUri: Uri? = null
+
+    private var movieId: Int? = null
 
     // We use activityViewModels so that all our fragments will use the *same instance* of viewModel
     private val viewModel : moviesViewModel by activityViewModels()
@@ -44,41 +48,72 @@ class EditMovieFragment : Fragment() { // binding is the connector between our U
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentEditMovieBinding.inflate(inflater,container,false)
+
+
+        _binding = FragmentEditMovieBinding.inflate(inflater, container, false)
 
         viewModel.chosenMovie.observe(viewLifecycleOwner) {
             binding.itemTitle.text = Editable.Factory.getInstance().newEditable(it.title)
-            binding.itemDescription.text = Editable.Factory.getInstance().newEditable(it.description)
+            binding.itemDescription.text =
+                Editable.Factory.getInstance().newEditable(it.description)
             binding.itemGenre.text = Editable.Factory.getInstance().newEditable(it.genre)
             binding.itemYearRelease.text = Editable.Factory.getInstance().newEditable(it.year)
             Glide.with(requireContext()).load(it.photo)
                 .into(binding.resultImage)
+            imageUri=Uri.parse(it.photo)
+            movieId = it.id
         }
 
-        // Adding the movie (with the data the user entered) to our local DB
+        // editing  the movie (with the data the user entered) to our local DB
         binding.finishBtn.setOnClickListener {
-            val movie  = Movie(binding.itemTitle.text.toString(),
-                binding.itemDescription.text.toString(),
-                binding.itemGenre.text.toString(),
-                binding.itemYearRelease.text.toString(),
-                imageUri.toString())
+            var emptyFields = ""
+            if (binding.itemTitle.text.toString().isEmpty()){
+                emptyFields += "${getString(R.string.title)}\n"
+            }
+            if (binding.itemDescription.text.toString().isEmpty()){
+                emptyFields += "${getString(R.string.description)}\n"
+            }
+            if (binding.itemGenre.text.toString().isEmpty()){
+                emptyFields += "${getString(R.string.genre)}\n"
+            }
+            if (binding.itemYearRelease.text.toString().isEmpty()){
+                emptyFields += "${getString(R.string.year)}\n"
+            }
+            if (imageUri?.toString().isNullOrEmpty()){
+                emptyFields += "${getString(R.string.image)}"
+            }
 
-            viewModel.updateMovie(movie)
+            if(emptyFields.isNotEmpty()){
+                val note = Snackbar.make(
+                    requireView(),
+                    "${getString(R.string.pls_fill)}\n$emptyFields", Toast.LENGTH_SHORT)
+                note.setTextMaxLines(6)
+                note.show()
+            }
+            // Adding the movie (with the data the user entered) to our local DB
+            else {
+                val movie = Movie(
+                    binding.itemTitle.text.toString(),
+                    binding.itemDescription.text.toString(),
+                    binding.itemGenre.text.toString(),
+                    binding.itemYearRelease.text.toString(),
+                    imageUri.toString()
+                )
 
-            // Going back to hone screen
-            findNavController().navigate(
-                R.id.action_editMovieFragment_to_showMoviesFragment
-                , bundleOf("movie" to movie)
-            )
+                movie.id = movieId!!
+                viewModel.updateMovie(movie)
 
+                // Going back to home screen
+                findNavController().navigate(
+                    R.id.action_editMovieFragment_to_showMoviesFragment, bundleOf("movie" to movie)
+                )
+            }
         }
-
         binding.resultImage.setOnClickListener {
             chooseImageLauncher.launch(arrayOf("image/*"))
         }
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
